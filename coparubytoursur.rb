f6688ab94726475211c2497a75cd662d7a0d19bc
Martin Aceto <maceto@backing-online.com>
2011-08-18 19:38:16 -0300

    implement datamapper

--- coparubytoursur.rb
+++ coparubytoursur.rb
@@ -1,5 +1,22 @@
 require "cuba"
 require "haml"
+require "datamapper"
+require "dm-sqlite-adapter"
+
+DataMapper.setup(:default, 'sqlite://db/development.db')
+DataMapper::Logger.new($stdout, :debug)
+
+class Player 
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+  property :mail, String
+  property :twitter, String
+
+end
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 Cuba.use Rack::Static, urls: [ "/public/styles", "/public/images" ]
 
@@ -18,7 +35,7 @@ Cuba.define do
   on post do
     on "players" do
       on param("player") do |player_attributes|
-        puts player['name']
+        Player.create(player_attributes)
         res.redirect "/"
       end
     end

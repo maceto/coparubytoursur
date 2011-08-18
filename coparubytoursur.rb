1d1fdf0eb1dc8226695f8ab9eed458928ad3863d
Martin Aceto <maceto@backing-online.com>
2011-08-18 19:00:30 -0300

    understanding cuba param magic

--- coparubytoursur.rb
+++ coparubytoursur.rb
@@ -9,10 +9,19 @@ Cuba.define do
   on get do
     # /
     on "" do
+      puts "fue get"
       res.write render('views/index.haml')
     end
-
   end
 
+  # only POST requests
+  on post do
+    on "players" do
+      on param("player") do |player_attributes|
+        puts player['name']
+        res.redirect "/"
+      end
+    end
+  end
 
 end
